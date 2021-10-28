544eda3187894d058c4a142dda2e2f89f6bc7151
tetsup <tetsup.xperia@gmail.com>
2021-10-28 19:10:44 +0900

    deviseインストール #2

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bootstrap', '~> 4.0.0'
 
 gem 'haml-rails', '~> 2.0'
+gem 'devise', '~> 4.8.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # mailer setting
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
