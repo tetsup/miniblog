e7396b5c1475345380fb85b72496bac8d7ddde23
tetsup <tetsup.xperia@gmail.com>
2021-10-31 05:34:59 +0900

    ユーザープロフィールページ追加 #2

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'timelines#index'
   post 'timelines', to: 'timelines#create'
+  resources :users, only: [:show]
 end
