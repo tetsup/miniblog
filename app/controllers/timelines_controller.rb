ac233dc8f547f5f4a27ee11505d78781d46dc6b0
tetsup <tetsup.xperia@gmail.com>
2021-11-01 19:32:17 +0900

    フォローした人のタイムラインのみを表示するページ追加 #3

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,6 +1,11 @@
 class TimelinesController < ApplicationController
   def index
-    @timelines = Timeline.joins(:user).select("timelines.*, users.username").all.order(id: "DESC")
+    if params[:query] == 'following'
+      @followings = current_user.following_users
+      @timelines = Timeline.where(user_id: @followings).or(Timeline.where(user_id: current_user.id)).joins(:user).select("timelines.*, users.username").all.order(id: "DESC")
+    else
+      @timelines = Timeline.joins(:user).select("timelines.*, users.username").all.order(id: "DESC")
+    end
     @timeline = Timeline.new
   end
 
