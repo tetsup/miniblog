454cd96605f1e1397e2f7d6fa719f202544108dc
tetsup <tetsup.xperia@gmail.com>
2021-11-02 12:22:45 +0900

    productionのみBASIC認証 #4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+  
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['MINIBLOG_BASIC_AUTH_USERNAME'] && password == ENV['MINIBLOG_BASIC_AUTH_PASSWORD']
+    end
+  end
 end
