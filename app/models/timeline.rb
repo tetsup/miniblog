5fb5f486827dd11f3d2c8ec69b40361d5dcd1954
tetsup <tetsup.xperia@gmail.com>
2021-11-10 07:21:38 +0900

    余計な配列展開削除

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -3,7 +3,7 @@ class Timeline < ApplicationRecord
   has_many :favorites, dependent: :destroy
   belongs_to :user
 
-  scope :following, -> (user){ where(user: [user] + [*user.following_users]) }
+  scope :following, -> (user){ where(user: [user] + user.following_users) }
   scope :sorted, -> { order(created_at: :desc) }
   scope :favorites_count, -> {
     joins(:favorites).
