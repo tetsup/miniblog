5ce65d738255abb0eeabf16dcb440cab1b7ef5f3
tetsup <tetsup.xperia@gmail.com>
2021-11-14 08:55:57 +0900

    添付画像拡張子制限 #7

--- app/uploaders/attached_image_uploader.rb
+++ app/uploaders/attached_image_uploader.rb
@@ -4,4 +4,8 @@ class AttachedImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def extension_whitelist
+    ['jpg', 'jpeg', 'gif', 'png']
+  end
 end
