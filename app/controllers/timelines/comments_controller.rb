6913fd216a7c3ac864f9350e1c2a52a372e3681c
tetsup <tetsup.xperia@gmail.com>
2021-11-24 00:08:35 +0900

    500エラーではなく、バリデーション結果を表示するように変更

--- app/controllers/timelines/comments_controller.rb
+++ app/controllers/timelines/comments_controller.rb
@@ -3,9 +3,12 @@ class Timelines::CommentsController < ApplicationController
 
   def create
     comment = Timeline.find(params[:timeline_id]).comments.build(comment_params.merge(user: current_user))
-    comment.save!
-    CommentMailer.send_mail(comment).deliver_now
-    redirect_to request.referrer
+    if comment.save
+      CommentMailer.send_mail(comment).deliver_now
+      redirect_to request.referrer, notice: 'コメント完了しました'
+    else
+      redirect_to request.referrer, alert: comment.errors.full_messages.join(', ')
+    end
   end
 
   private
