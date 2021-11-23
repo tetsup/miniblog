5a5d4f44d4daf9d8659153cfbf21d5bd183e8841
tetsup <tetsup.xperia@gmail.com>
2021-11-23 23:17:36 +0900

    urlとhidden_fieldにuser_idが重複していたのを削除

--- app/controllers/users/follows_controller.rb
+++ app/controllers/users/follows_controller.rb
@@ -2,23 +2,19 @@ class Users::FollowsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    current_user.followings.create!(follow_params)
+    current_user.followings.create!(followed_user_id: params[:user_id])
     redirect_to_user_page
   end
 
   def destroy
-    current_user.followings.find_by_followed_user_id(follow_params[:followed_user_id]).destroy!
+    current_user.followings.find_by_followed_user_id(params[:user_id]).destroy!
     redirect_to_user_page
   end
 
   private
 
-  def follow_params
-    params.require(:follow).permit(:followed_user_id)
-  end
-
   def redirect_to_user_page
-    redirect_to user_path(follow_params[:followed_user_id])
+    redirect_to user_path(params[:user_id])
   end
 
 end
