432a3933be9a6dfe8c4aeba5db92696ddcb3c1bd
tetsup <tetsup.xperia@gmail.com>
2021-11-26 22:30:24 +0900

    ActiveRecord::Relation以外を返すscopeをクラスメソッドに変更

--- app/mailers/favorite_ranking_mailer.rb
+++ app/mailers/favorite_ranking_mailer.rb
@@ -1,6 +1,6 @@
 class FavoriteRankingMailer < ApplicationMailer
   def send_mail
-    @ranking = Timeline.eager_load(:user).preload(:favorite_users).favorites_rank(10, Time.zone.yesterday)
+    @ranking = Timeline.eager_load(:user).preload(:favorite_users).find(Favorite.get_favorites_rank(10, Time.zone.yesterday))
     mail(
       to: User.pluck(:email),
       subject: '昨日のいいね！ランキング'
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -6,4 +6,12 @@ class Favorite < ApplicationRecord
   scope :filter_by_date, -> (date){
     where(created_at: date.beginning_of_day..date.end_of_day)
   }
+
+  def self.get_favorites_rank(num, date)
+    filter_by_date(date).
+    group(:timeline_id).
+    order('count(timeline_id) desc').
+    limit(num).
+    pluck(:timeline_id)
+  end
 end
