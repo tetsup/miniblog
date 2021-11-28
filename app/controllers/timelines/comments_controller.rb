7a6174c79ff68886f6d8e3e94ce9c29d414cb7d1
tetsup <tetsup.xperia@gmail.com>
2021-11-28 15:28:40 +0900

    コメント投稿時メール送信の配置変更

--- app/controllers/timelines/comments_controller.rb
+++ app/controllers/timelines/comments_controller.rb
@@ -4,7 +4,7 @@ class Timelines::CommentsController < ApplicationController
   def create
     comment = Timeline.find(params[:timeline_id]).comments.build(comment_params.merge(user: current_user))
     if comment.save
-      CommentMailer.send_mail(comment).deliver_now
+      UserMailer.comment(comment).deliver_later
       redirect_to request.referrer, notice: 'コメント完了しました'
     else
       redirect_to request.referrer, alert: comment.errors.full_messages.join(', ')
