65509dd0987c8c02c7fc5897b9887a02212f6a3b
tetsup <tetsup.xperia@gmail.com>
2021-11-28 15:33:32 +0900

    current_user起点でのbuildに変更

--- app/controllers/timelines/comments_controller.rb
+++ app/controllers/timelines/comments_controller.rb
@@ -2,7 +2,7 @@ class Timelines::CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    comment = Timeline.find(params[:timeline_id]).comments.build(comment_params.merge(user: current_user))
+    comment = current_user.comments.build(comment_params.merge(timeline_id: params[:timeline_id]))
     if comment.save
       UserMailer.comment(comment).deliver_later
       redirect_back fallback_location: timelines_path, notice: 'コメント完了しました'
