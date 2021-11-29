81b2da0fba2aac6b9588c67eaf8263d91571de56
tetsup <tetsup.xperia@gmail.com>
2021-11-29 22:31:51 +0900

    rubocopに準拠

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,15 +3,15 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :timeline
 
-  scope :filter_by_date, -> (date){
+  scope :filter_by_date, ->(date) {
     where(created_at: date.all_day)
   }
 
   def self.get_favorites_rank(lower_limit, date)
-    filter_by_date(date).
-    group(:timeline_id).
-    order('count(timeline_id) desc').
-    limit(lower_limit).
-    pluck(:timeline_id)
+    filter_by_date(date)
+      .group(:timeline_id)
+      .order('count(timeline_id) desc')
+      .limit(lower_limit)
+      .pluck(:timeline_id)
   end
 end
