5cfe07674c5828385ca5a7d743e2dfc36c683b87
tetsup <tetsup.xperia@gmail.com>
2021-11-29 22:08:51 +0900

    1日分をall_dayに書き換え

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -4,7 +4,7 @@ class Favorite < ApplicationRecord
   belongs_to :timeline
 
   scope :filter_by_date, -> (date){
-    where(created_at: date.beginning_of_day..date.end_of_day)
+    where(created_at: date.all_day)
   }
 
   def self.get_favorites_rank(num, date)
